95edb397a61f5e0f84f0857cdc1b2a8e7216661f
Ariel Granato <arielgranatob@gmail.com>
2020-05-21 13:19:59 -0300

    Gráfico de casos confirmados na página inicial do painel adicionado, dados estáticos ainda.

--- app/Views/login/painel.php
+++ app/Views/login/painel.php
@@ -93,22 +93,53 @@
       </nav>
 
       <!-- conteudo -->
-      <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Olá, <?= session()->get('firstname') ?>, bem-vindo ao Painel de Administração</h1>
+      <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+          <h1 class="h2">Gráfico de Casos Confirmados em Rio Pomba - MG</h1>
+          <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group mr-2">
+              <button class="btn btn-sm btn-outline-secondary">Exportar em PDF</button>
+            </div>
+          </div>
         </div>
-      </main>
+        <canvas class="my-4" id="myChart" width="900" height="380"></canvas>
     </div>
   </div>
-
-
-
-
-
+  
   <script src="../assets/bootstrap.bundle.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
   <script src="../assets/dashboard.js"></script>
-
+  <!-- Graphs -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+  <script>
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ["04/05", "06/05", "09/05", "10/05", "13/05", "17/05", "23/05"],
+        datasets: [{
+          data: [0,1,3,5,9,15,24],
+          lineTension: 0,
+          backgroundColor: 'transparent',
+          borderColor: '#007bff',
+          borderWidth: 4,
+          pointBackgroundColor: '#007bff'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: false
+            }
+          }]
+        },
+        legend: {
+          display: false,
+        }
+      }
+    });
+  </script>
 </body>
 
 </html>
